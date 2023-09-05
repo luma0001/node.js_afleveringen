654f4ad3833cf2b74e76627e7eb94ab97c7c8b08
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-09-05 16:14:54 +0200

    needless cleanData function added

--- frontend/restFunctions.js
+++ frontend/restFunctions.js
@@ -1,6 +1,19 @@
 async function fetchArtists() {
   const promise = await fetch("../Backend/artists.json");
-  return promise.json();
+  const data = await promise.json();
+
+  return cleanData(data);
+}
+
+function cleanData(data) {
+  //hvis det er et objekt med objekter - skal vi så bruger
+  const objectArray = [];
+  for (const object in data) {
+    const artistObject = data[object];
+    objectArray.push(artistObject);
+  }
+  console.log(objectArray);
+  return objectArray;
 }
 
 export { fetchArtists };
